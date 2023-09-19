9a2e9e68c030dd61d23b62e1a8c009928d5207f5
Noorain Tahir <noorain.tahirr@gmail.com>
2023-09-19 11:29:29 +0500

    Multiplication function of Calculator with flask

---
+++ env/mul.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app= Flask(__name__)
+@app.route('/mul/<int:variable1>/<int:variable2>')
+def multi(a,b):
+    return a*b
+
+@app.route('/')
+def hi():
+    return 'Welcome to Calculator!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
